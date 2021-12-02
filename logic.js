252afe29ad4ca9735ba40a1d4acbf413aec10ad5
Johan Jig <johan.jigsved@gmail.com>
2021-12-02 16:08:23 +0100

    Second commit

--- logic.js
+++ logic.js
@@ -14,22 +14,78 @@ function divide (num1, num2) {
     return num1 / num2;
 }
 
-function operate (operator, num1, num2) {
+function operate (num1,operator, num2) {
     switch (operator) {
-        case 0:
+        case "+":
             return add(num1, num2);
             break;
 
-        case 1:
+        case "-":
             return subtract(num1, num2);
             break;
 
-        case 2: 
+        case "*": 
             return multiply(num1, num2);
             break;
 
-        case 3:
+        case "/":
             return divide(num1, num2);
             break;
      }
 }
+
+function numPress (num1) {
+    return num1;
+}
+
+const numButtonRef = document.querySelectorAll(".button-num");
+const displayRef = document.querySelector(".calculator-display-num");
+let displayVal = '';
+
+numButtonRef.forEach(function(element){
+    element.addEventListener('click', ()=> {
+        displayVal = displayVal + element.innerHTML;
+        displayRef.textContent = displayVal;
+    })
+});
+    
+const eqButtonRef = document.querySelector(".button-equals");
+eqButtonRef.addEventListener('click', () => {
+   let displayValArray = displayVal.split(" ");
+    let displayValtemp;
+
+    while(displayValArray[1]) {
+        displayValsum = operate(parseFloat(displayValArray[0]),displayValArray[1], parseFloat(displayValArray[2]));
+        displayValArray.splice(0,3);
+        displayValArray.unshift(displayValsum);
+        
+    }
+    displayVal = displayValsum;
+    displayValsum = '';
+   displayRef.textContent = displayVal, + " ";
+});
+
+const clearButtonRef = document.querySelector(".button-clear");
+clearButtonRef.addEventListener('click', () => {
+    displayVal = '';
+    displayRef.textContent = displayVal;
+})
+
+const clearButtonDel = document.querySelector(".button-del");
+clearButtonDel.addEventListener('click', () => {
+    displayVal = displayVal.substring(0, displayVal.length-1);
+    displayRef.textContent = displayVal;
+});
+
+const dotButtonRef = document.querySelector(".button-dot");
+dotButtonRef.addEventListener('click', () => {
+    for (i=displayVal.length(); i < 0; i--) {
+        console.log(i);
+    }
+    
+    if (displayVal.slice(-1) !== ".") {
+        displayVal = displayVal + dotButtonRef.innerHTML;
+        displayRef.textContent = displayVal;
+
+    }
+});
